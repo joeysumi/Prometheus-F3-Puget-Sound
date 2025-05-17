f2fc27ae7f80876d6e5df79daaf62a656a292909
Joseph Sumi <josephsumi@gmail.com>
2025-05-17 15:06:18 -0600

    added lambda_function for aws lambda

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+from prometheus import Prometheus
+
+
+def lambda_handler(event, context):
+    creds = {
+        "host": os.environ.get("host"),
+        "database": os.environ.get("database"),
+        "username": os.environ.get("username"),
+        "password": os.environ.get("password"),
+    }
+    app = Prometheus(credentials=creds)
+    app.run()
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Prometheus Run")
+    }
